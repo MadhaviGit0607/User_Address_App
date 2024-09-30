a1ac377586893ce04981f50054943ad4060250dd
MadhaviGit0607 <162568991+MadhaviGit0607@users.noreply.github.com>
2024-09-30 16:04:53 +0530

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,29 +1,28 @@
-let FormEl=document.getElementById('registration-form');
-FormEl.addEventListener('submit', function(event) {
-    
+let FormEl = document.getElementById('registration-form');
+
+FormEl.addEventListener('submit', async function(event) {
     event.preventDefault();
 
     const name = document.getElementById('name').value;
     const address = document.getElementById('address').value;
 
-    fetch('/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ name, address })
-    })
-    .then(response => {
-        if (response.ok) {
-                 return response.json();
-        } else {
+    try {
+        const response = await fetch('/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, address })
+        });
+
+        if (!response.ok) {
             throw new Error('User registration failed.');
         }
-    })
-    .then(data => {
+
+        const data = await response.json();
         document.getElementById('response').innerText = data.message;
-    })
-    .catch(error => {
+
+    } catch (error) {
         document.getElementById('response').innerText = error.message;
-    });
+    }
 });
